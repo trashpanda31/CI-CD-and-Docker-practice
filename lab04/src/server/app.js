4767f8377e4dcfc0dadc187e74314c4ed15ed913
Eliass <e.smailik@yandex.com>
2025-10-11 19:03:12 +0300

    fix/lab04

--- lab04/src/server/app.js
+++ lab04/src/server/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   console.log("req.path", req.path);
-  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = app;
